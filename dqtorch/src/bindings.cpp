55004ed5ff9e1ca1a42c76dd162b474e0c634d90
Marisha <bnmay19@gmail.com>
2024-11-15 17:52:48 -0600

    More AutoFormatting

--- dqtorch/src/bindings.cpp
+++ dqtorch/src/bindings.cpp
@@ -2,7 +2,8 @@
 
 #include "quaternion.h"
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
     m.def("quaternion_mul_forward", &quaternion_mul_forward, "quaternion multiplication forward (CUDA)");
     m.def("quaternion_mul_backward", &quaternion_mul_backward, "quaternion multiplication backward (CUDA)");
     m.def("quaternion_mul_backward_backward", &quaternion_mul_backward_backward, "quaternion multiplication backward (CUDA)");
